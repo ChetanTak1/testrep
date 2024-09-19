6fbbc0e0c55318c2a1b7104b874d78624f65c3f7
ChetanTak1 <chetantak2002@gmail.com>
2024-09-19 20:05:57 +0530

    lesgo

---
+++ assgn.js
@@ -0,0 +1,24 @@
+// let name="mithun"
+// let email=9
+// let age="urdad"
+
+
+
+// if(typeof(age) != "number"){
+//     console.log("ples enter valid age")
+// }
+// if(typeof(name) != "string"){
+//     console.log("ples enter valid namw")
+// }
+// if(typeof(email) != "string"){
+//     console.log("ples enter valid email")
+// }
+
+const arr=[1,2,-1,3,-4,-2,4]
+
+for(i=0;i<arr.length;i++){
+    if(arr[i]>0){
+        console.log(arr[i])
+        
+    }
+}
